516307e0419250758e05c7c14ca0902b8208b632
talha-ahmed-1 <talhaahmedashraf@gmail.com>
2023-03-13 10:57:51 +0500

    examples fixes

--- examples/example.py
+++ examples/example.py
@@ -10,8 +10,12 @@ parser = ParseAST()
 parser.parser("/home/talha/chiV/examples/test.ast")
 module = parser.get_top_module()
 portlist = parser.get_portlist()
+# print(parser.get_instances())
+# print(parser.get_modules())
+# print(module)
+# print(portlist)
 
 bb_gen = BlackBoxGen()
-bb = bb_gen.gen_bb(module, portlist)
+bb = bb_gen.gen_bb(module[0], portlist)
 for i in bb:
     print(i)
